1cd7d3ae879458da41d3e325ab0875daa11dbaba
Vijay <vijaythevirtue@gmail.com>
2020-12-23 10:47:14 +0530

    icon Styling - DONE

--- src/Components/MyButton.jsx
+++ src/Components/MyButton.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
 import Button from '@material-ui/core/Button';
 import "./Styles/ButtonStyles.css"
+import { FcCollaboration } from "react-icons/fc";
+import { FcLandscape } from "react-icons/fc";
+import Grid from '@material-ui/core/Grid';
 
 class MyButtonClass extends Component {
     render() {
         return (
             <div className="d-flex align-items-center justify-content-center  customHeight">
-                <Button className="btnStyles"><span className="btnTxtColor">Vijay Creations</span></Button>
+                <Grid>
+                    <Grid item>
+                        <div>
+                            <Button className="btnStyles"><span className="btnTxtColor">Vijay Creations</span></Button>
+                        </div>
+                    </Grid>
+                    <Grid item>
+                        <Grid container direction="row"
+                            justify="space-around"
+                            alignItems="center">
+                            <Grid item>
+                                <div className="my-5">
+                                    <FcCollaboration size={70}></FcCollaboration>
+                                </div>
+
+                            </Grid>
+                            <Grid item>
+                                <div className="my-5">
+                                    <FcLandscape size={70}></FcLandscape>
+                                </div>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+
+
             </div>
         )
     }
